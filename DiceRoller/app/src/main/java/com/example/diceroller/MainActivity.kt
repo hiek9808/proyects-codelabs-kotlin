8022e313199fd40378be84cc174b20d7c503812d
Kervin Herrera <kervin9808@gmail.com>
2019-11-04 15:47:45 -0500

    first commit

---
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * lateinit significa que la variable sera asignado mas tarde
+     */
+    lateinit var diceImage: ImageView
+    lateinit var diceImage2: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image_2)
+
+
+
+        rollButton.setOnClickListener { rollDice() }
+
+    }
+
+    private fun rollDice() {
+
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+
+        //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getRandomDiceImage(): Int{
+        val randomInt = Random().nextInt(6) + 1
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+    }
+
+}
